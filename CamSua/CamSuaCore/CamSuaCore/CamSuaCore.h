b361d4ca69fdf7850c83f8139d8972cefefc7dc6
TheSon <theson.camsua@gmail.com>
2019-05-24 11:53:47 +0700

    - Updated pod

---
+++ CamSua/CamSuaCore/CamSuaCore/CamSuaCore.h
@@ -0,0 +1,19 @@
+//
+//  CamSuaCore.h
+//  CamSuaCore
+//
+//  Created by Jacob on 5/24/19.
+//  Copyright © 2019 CamSua Private Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CamSuaCore.
+FOUNDATION_EXPORT double CamSuaCoreVersionNumber;
+
+//! Project version string for CamSuaCore.
+FOUNDATION_EXPORT const unsigned char CamSuaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CamSuaCore/PublicHeader.h>
+
+
---
+++ CamSua/Pods/Target Support Files/Pods-CamSuaCore/Pods-CamSuaCore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CamSuaCore : NSObject
+@end
+@implementation PodsDummy_Pods_CamSuaCore
+@end
