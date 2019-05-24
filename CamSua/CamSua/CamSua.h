9e6d010aae1616ad8b231ad563ce67c35495ee8b
Son Bui <son.bui@pizzahut.io>
2019-05-24 10:35:55 +0700

    Init project

---
+++ CamSua/CamSua/CamSua.h
@@ -0,0 +1,19 @@
+//
+//  CamSua.h
+//  CamSua
+//
+//  Created by Jacob on 5/24/19.
+//  Copyright © 2019 CamSua Private Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CamSua.
+FOUNDATION_EXPORT double CamSuaVersionNumber;
+
+//! Project version string for CamSua.
+FOUNDATION_EXPORT const unsigned char CamSuaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CamSua/PublicHeader.h>
+
+
